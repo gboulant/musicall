764a904e6b5f503d8ad5420442f2d382d2f4e876
Guillaume BOULANT <guillaume.boulant@edf.fr>
2025-07-07 19:09:18 +0200

    fix typing error

--- cmds/graphwave/waveplot.py
+++ cmds/graphwave/waveplot.py
@@ -9,16 +9,18 @@ lib.SineWave.argtypes = [c_double, c_double, c_double, POINTER(c_double)]
 lib.SquareWave.argtypes = [c_double, c_double, c_double, POINTER(c_double)]
 lib.KarplusStrongWave.argtypes = [c_double, c_double, c_double, POINTER(c_double)]
 
-def makewave(f,a,d, wavefct)-> array[float]:
+from typing import List
+
+def makewave(f,a,d, wavefct) -> List[float]:
     N = lib.WaveSize(d)
     out = array('d', [0 for i in range(N)]) 
     out_ptr = (c_double * len(out)).from_buffer(out)
     wavefct(f,a,d, out_ptr)
     return out
 
-def SineWave(f,a,d) -> array[float]: return makewave(f,a,d, lib.SineWave)
-def SquareWave(f,a,d) -> array[float]: return makewave(f,a,d, lib.SquareWave)
-def KarplusStrongWave(f,a,d) -> array[float]: return makewave(f,a,d, lib.KarplusStrongWave)
+def SineWave(f,a,d) -> List[float]: return makewave(f,a,d, lib.SineWave)
+def SquareWave(f,a,d) -> List[float]: return makewave(f,a,d, lib.SquareWave)
+def KarplusStrongWave(f,a,d) -> List[float]: return makewave(f,a,d, lib.KarplusStrongWave)
 
 import matplotlib.pyplot as plt
 def plottimeseries(t,s):
