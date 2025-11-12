12573e297b54ac9e01a3a5aa2977d63168310dbd
Guillaume BOULANT <guillaume.boulant@edf.fr>
2025-11-12 10:06:55 +0100

    d11.guitarneck: save the frequencies in a CSV file

--- demos/d11.guitarneck/main.go
+++ demos/d11.guitarneck/main.go
@@ -17,17 +17,13 @@ var stringNumbers = []guitar.StringNumber{
 	guitar.Mi1,
 }
 
-func NoteName(stringNumber guitar.StringNumber, fretNumber guitar.FretNumber) string {
-	note := guitar.Note{StringNum: stringNumber, FretNum: fretNumber}
-	return note.Name()
-}
-
 // demo01_printnames print all notes for all couple (string, fret)
 func demo01_printnames() error {
+	var fretNumber guitar.FretNumber
 	for _, stringNumber := range stringNumbers {
-		for fretNumber := range 8 {
-			name := NoteName(stringNumber, guitar.FretNumber(fretNumber))
-			fmt.Printf("Note(s=%d, f=%d): %s\n", stringNumber, fretNumber, name)
+		for fretNumber = range 8 {
+			note := guitar.Note{StringNum: stringNumber, FretNum: fretNumber}
+			fmt.Printf("Note(s=%d, f=%d): %s\n", stringNumber, fretNumber, note.Name())
 		}
 	}
 	return nil
@@ -48,27 +44,19 @@ func demo02_guitarneck() error {
 	for _, stringNumber := range stringNumbers {
 		var line string = fmt.Sprintf("S%d | ", stringNumber)
 		for fretNumber := range nbfrets {
-			name := NoteName(stringNumber, guitar.FretNumber(fretNumber))
-			line += fmt.Sprintf("%-6s", name)
+			note := guitar.Note{StringNum: stringNumber, FretNum: guitar.FretNumber(fretNumber)}
+			line += fmt.Sprintf("%-6s", note.Name())
 		}
 		fmt.Println(line)
 	}
 	return nil
 }
 
-// demo03_guitarneck_ToCSV does like demo02 but records the result in a
-// CSV file. The cell (icol,iline) contains the note of the string
-// s=iline for the fret f=icol+1. The header is:
-//
-// Sn; F0; F1; ...; F16
-//
-// where the column Sn contains the number of the guitar string.
-func demo03_guitarneck_ToCSV() error {
-	nbfrets := 16
-
-	// -------------------------------------------
-	// STEP 01: creating the records
-	var records [][]string = make([][]string, len(stringNumbers)+1)
+// makerecords creates a dataset of records prepared to be saved in CSV formated
+// file. The data in a record cell is a string created from a Note using the
+// function note2string.
+func makerecords(nbfrets int, note2string func(n guitar.Note) string) (records [][]string) {
+	records = make([][]string, len(stringNumbers)+1)
 
 	// Header line
 	records[0] = make([]string, nbfrets+1)
@@ -78,18 +66,19 @@ func demo03_guitarneck_ToCSV() error {
 	}
 
 	// Lines of data
-	for _, sn := range stringNumbers {
-		records[sn] = make([]string, nbfrets+1)
-		records[sn][0] = fmt.Sprintf("S%d", sn)
+	for _, stringNumber := range stringNumbers {
+		records[stringNumber] = make([]string, nbfrets+1)
+		records[stringNumber][0] = fmt.Sprintf("S%d", stringNumber)
 		for fretNumber := range nbfrets {
-			name := NoteName(sn, guitar.FretNumber(fretNumber))
-			records[sn][fretNumber+1] = name
+			note := guitar.Note{StringNum: stringNumber, FretNum: guitar.FretNumber(fretNumber)}
+			records[stringNumber][fretNumber+1] = note2string(note)
 		}
 	}
+	return records
+}
 
-	// -------------------------------------------
-	// STEP 02: save the records in CSF formated file
-	csvpath := "output.guitarneck.csv"
+// saverecords saves the dataset of records into a CSV formated file
+func saverecords(records [][]string, csvpath string) error {
 	csvfile, err := os.OpenFile(csvpath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
 	if err != nil {
 		return err
@@ -111,8 +100,52 @@ func demo03_guitarneck_ToCSV() error {
 	return nil
 }
 
+// demo03_guitarneck_NameToCSV creates a CSV formated file containing the names
+// of the notes for each guitar strings (lines) and frets (columns). The header
+// is:
+//
+// Sn; F0; F1; ...; F16
+//
+// where the column Sn contains the number of the guitar string (1 for the Mi3
+// and 6 for the Mi1).
+func demo03_guitarneck_NameToCSV() error {
+	nbfrets := 16
+
+	// -------------------------------------------
+	// STEP 01: creating the records
+	note2string := func(n guitar.Note) string {
+		return n.Name()
+	}
+	records := makerecords(nbfrets, note2string)
+
+	// -------------------------------------------
+	// STEP 02: save the records in CSF formated file
+	csvpath := "output.guitarneck.names.csv"
+	return saverecords(records, csvpath)
+}
+
+// demo04_guitarneck_FreqToCSV creates a CSV formated file containing the
+// frequencies of the notes for each guitar strings (lines) and frets (columns).
+// The header
+func demo04_guitarneck_FreqToCSV() error {
+	nbfrets := 16
+
+	// -------------------------------------------
+	// STEP 01: creating the records
+	note2string := func(n guitar.Note) string {
+		return fmt.Sprintf("%.1f", n.Frequency())
+	}
+	records := makerecords(nbfrets, note2string)
+
+	// -------------------------------------------
+	// STEP 02: save the records in CSF formated file
+	csvpath := "output.guitarneck.frequencies.csv"
+	return saverecords(records, csvpath)
+}
+
 func main() {
-	//demo01_printnames()
+	demo01_printnames()
 	demo02_guitarneck()
-	demo03_guitarneck_ToCSV()
+	demo03_guitarneck_NameToCSV()
+	demo04_guitarneck_FreqToCSV()
 }
